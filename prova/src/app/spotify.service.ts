ca146996cd6bc525a7b31fda1e1fa540bd5dee8c
giorgia15 <bozzetti.giorgia@iisgalvanimi.edu.it>
2022-03-29 11:07:00 +0000

    fine 01.1

--- prova/src/app/spotify.service.ts
+++ prova/src/app/spotify.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
@@ -12,13 +14,20 @@ export class SpotifyService {
 
   searchTrack(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQDeCREbA9mJEzaIP2yuqcIyOI6ROdvMC3iw68WFVDkD6h-FITKzPxNr7TR2seFBllZ-ejTphXORrdCLF3JM3KCVjBVJfNjCrDlhbom6VJxZKVibFPthKuegK-weops80HABa_OrM6AEXBiPeClK7JjQFyuD9Ec'
-    });
+    const headers = new HttpHeaders({Authorization: environment.oauthToken});
 
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
  //Ritorno un observable ai componenti che richiedono il servizio
+ 
   }
+  getTrack(id: string) {
+    const url = `https://api.spotify.com/v1/tracks/${id}`;
+    const headers = new HttpHeaders({Authorization: environment.oauthToken});
+    
+    return this.http.get(url, { headers });
+  }
+  
+
+
 }
--- prova/src/environments/environment.ts
+++ prova/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  oauthToken: "Bearer  BQB5TCAXKeg-dxMesCRaQW8I2e7NzMJOdkzMqN5MscTGNGEJdx-MSBNXv-rHJX-yjOvnSQpsvGyRLR4cxlgpCtL34kbfy3LLNsxKZQr9t4WGeXpGk1k5FeJ23OKeyfmUW4opXtyefhZoCWyht18FD-WgY0kgQqA"
 };
 
 /*
